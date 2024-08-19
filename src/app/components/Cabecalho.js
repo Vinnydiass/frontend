ba09240daff9972d043f67ddc12239d44b4f3104
Vinnydiass <vinynavidareal@gmail.com>
2024-08-19 20:46:15 -0300

    first commit

---
+++ src/app/components/Cabecalho.js
@@ -0,0 +1,12 @@
+export default function Cabecalho(){
+    return (
+        <>
+            <div>
+                <h1>Cabeçalho</h1>
+                <p>Isto é o Cabeçalho da minha página</p>
+            </div>
+        
+
+        </>
+    )
+}
---
+++ src/app/fundamentos/page.js
@@ -0,0 +1,47 @@
+import { Alert, Container } from "react-bootstrap";
+import Cabecalho from "../components/Cabecalho";
+import Link from "next/link";
+
+export default function fundamentos(){
+
+
+   
+
+    return(
+        <>
+        <Cabecalho/>
+
+        <Container>
+        <Alert>
+            Atenção
+        </Alert>
+        <a href="/">Pagina inicial</a> <br/>
+        <Link href="/">Pagina inicial</Link>
+      
+        <h1>Fundamentos</h1>
+        <p>DWDW</p>
+        <Cabecalho/>
+        </Container>
+        </>
+        
+    )
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
